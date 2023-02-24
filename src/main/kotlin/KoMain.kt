0bdf79470113d36fe5e52558f00ff0052e8f6afc
bmsk <vv99911@gmail.com>
2023-02-24 17:27:21 +0900

    [Add] Inflearn 2_11

--- src/main/kotlin/KoMain.kt
+++ src/main/kotlin/KoMain.kt
@@ -1,15 +1,3 @@
 fun main(args: Array<String>){
-    val input = readText().split("\n")
-    val n = input[0].toInt()
-    val list = IntArray(n)
-    for (i in 1..n) {
-        list[i-1] = input[i].toInt()
-    }
-    list.sort()
-    val sb = StringBuilder()
 
-    for (i in 0 until n) {
-        sb.append(list[i]).append('\n')
-    }
-    print(sb)
 }
---
+++ src/main/kotlin/inflearn/java/_2_11_임시반장_정하기.java
@@ -0,0 +1,46 @@
+package inflearn.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2_11_임시반장_정하기 {
+    public static int solution(int n, int[][] inputs) {
+        int max = Integer.MIN_VALUE;
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++) {
+            int sum = 0;
+            for(int j = 1; j <= n; j++) {
+                for(int k = 1; k <= 5; k++) {
+                    if(inputs[i][k] == inputs[j][k]) {
+                        sum++;
+                        break;
+                    }
+                }
+            }
+            if(sum > max) {
+                max = sum;
+                answer = i;
+            }
+        }
+        return answer;
+    }
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[][] inputs = new int[n + 1][6];
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j < 6; j++) {
+                inputs[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = solution(n, inputs);
+        System.out.print(result);
+    }
+}
