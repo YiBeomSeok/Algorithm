e1dfb6045086405d2b7e7b2b45d6c21f65366cec
bmsk <vv99911@gmail.com>
2023-03-03 16:04:30 +0900

    [Add] Inflearn - 3_4, 3_5, 3_6

---
+++ src/main/kotlin/inflearn/java/_3_5_연속된_자연수의_합.java
@@ -0,0 +1,26 @@
+package inflearn.java;
+
+import java.util.Scanner;
+
+public class _3_5_연속된_자연수의_합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int lt = 1;
+        int sum = lt;
+        int answer = 0;
+        for(int rt = 2; rt <= n / 2 + 1; rt++) {
+            sum += rt;
+            if(sum == n) answer++;
+            while(sum >= n) {
+                sum -= lt++;
+                if(sum == n) answer++;
+            }
+        }
+        return answer;
+    }
+}
