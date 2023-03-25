dcea17c95f5db4a9645e3868ee67cc99424398b5
bmsk <vv99911@gmail.com>
2023-03-25 22:09:30 +0900

    update

--- 프로그래머스/lv2/72412. 순위 검색/스래기.kt
+++ 프로그래머스/lv2/72412. 순위 검색/스래기.kt
@@ -1,71 +1,125 @@
-import java.util.StringTokenizer
+import java.io.*
+import java.util.*
+
+// 17:00
 class Solution {
-    var count=0
-    fun lower_bound(arrNum : ArrayList<Int>, score :Int): Int{
-
-        var low = 0
-        var high = arrNum.size-1
-        while(low<=high){
-            var mid= (low+high)/2
-            if(arrNum[mid]<score){
-                low = mid+1
-            }else{
-                high = mid-1
-            }
-        }
-        return arrNum.size-low
-    }
-    fun dfs(infoMap : MutableMap<String,ArrayList<Int>>,word : Array<Array<String>>,score : Int,sentence : String, cnt : Int){
-        if(cnt==4){
-            count++
-            if(infoMap[sentence]==null){
-                infoMap.put(sentence,ArrayList<Int>())
-            }
-            infoMap[sentence]?.add(score)
-            return
-        }
-            dfs(infoMap,word,score,sentence+word[cnt][0],cnt+1)
-            dfs(infoMap,word,score,sentence+word[cnt][1],cnt+1)
-    }
-
+    val applicants = HashMap<String, MutableList<Int>>()
+    lateinit var answer: IntArray
     fun solution(info: Array<String>, query: Array<String>): IntArray {
-        var answer = IntArray(query.size)
-        var infoMap = mutableMapOf<String,ArrayList<Int>>()
-        for(sen in info){
-            val tk = StringTokenizer(sen)
-            val word= Array<Array<String>>(4){Array<String>(2){"-"} }
-            for(i in 0 until 4){
-                word[i][0]=tk.nextToken()
+        answer = IntArray(query.size) { 0 }
+
+        dfs("0", 1)
+        dfs("1", 1)
+        dfs("2", 1)
+
+        info.forEach {
+            val st = StringTokenizer(it)
+            val lang = when (st.nextToken()) {
+                "cpp" -> 0
+                "java" -> 1
+                "python" -> 2
+                else -> -1
             }
-            val score = Integer.parseInt(tk.nextToken())
-            dfs(infoMap,word,score,"",0)
-            count=0
-        }
-        for(i in infoMap){
-            i.value.sort()
-        }
-
-        //쿼리별 검색
-        for(i in query.indices){
-            val tk = StringTokenizer(query[i])
-            var sentence =""
-
-            for(j in 0 until 8){
-                val str = tk.nextToken()
-                if(j%2==0)
-                sentence+=str
-                else if(j==7){
-                    if(infoMap[sentence]==null){
-                        answer[i]=0
-                    }
-                    else{
-                        answer[i] = lower_bound(infoMap[sentence]!!,Integer.parseInt(str))
-                    }
-                }
-
+            val dev = when (st.nextToken()) {
+                "backend" -> 0
+                "frontend" -> 1
+                else -> -1
             }
+            val expert = when (st.nextToken()) {
+                "junior" -> 0
+                "senior" -> 1
+                else -> -1
+            }
+            val food = when (st.nextToken()) {
+                "chicken" -> 0
+                "pizza" -> 1
+                else -> -1
+            }
+            applicants["$lang$dev$expert$food"]?.add(st.nextToken().toInt())
+        }
 
+        var index = 0
+        query.forEach { qr ->
+            val split = qr.split(" and ", " ")
+            val lang = when(split[0]) {
+                "cpp" -> 0
+                "java" -> 1
+                "python" -> 2
+                else -> "-"
+            }
+            val dev = when(split[1]) {
+                "backend" -> 0
+                "frontend" -> 1
+                else -> "-"
+            }
+            val expert = when(split[2]) {
+                "junior" -> 0
+                "senior" -> 1
+                else -> "-"
+            }
+            val food = when(split[3]) {
+                "chicken" -> 0
+                "pizza" -> 1
+                else -> "-"
+            }
+            val score = split[4].toInt()
+
+            val cmd = StringBuilder("$lang$dev$expert$food")
+            dfs2(cmd, 0, score, index)
+            index++
         }
         return answer
     }
+
+    fun dfs(filter: String, depth: Int) {
+        if (depth == 4) {
+            applicants[filter] = mutableListOf()
+        } else {
+            dfs("${filter}0", depth + 1)
+            dfs("${filter}1", depth + 1)
+        }
+    }
+
+    fun dfs2(filter: StringBuilder, depth: Int, score: Int, index: Int) {
+        if (depth == 4) {
+            applicants[filter.toString()]?.forEach {
+                if (it >= score)
+                    answer[index]++
+            }
+        } else {
+            if (filter[depth] == '-') {
+                var next = StringBuilder(filter.toString())
+                next[depth] = '0'
+                dfs2(next, depth + 1, score, index)
+                next[depth] = '1'
+                dfs2(next, depth + 1, score, index)
+                if (depth == 0) {
+                    next[depth] = '2'
+                    dfs2(next, 1, score, index)
+                }
+            } else {
+                dfs2(filter, depth + 1, score, index)
+            }
+        }
+    }
+}
+
+fun main() {
+    val info = arrayOf(
+        "java backend junior pizza 150",
+        "python frontend senior chicken 210",
+        "python frontend senior chicken 150",
+        "cpp backend senior pizza 260",
+        "java backend junior chicken 80",
+        "python backend senior chicken 50",
+    )
+    val query = arrayOf(
+        "java and backend and junior and pizza 100",
+        "python and frontend and senior and chicken 200",
+        "cpp and - and senior and pizza 250",
+        "- and backend and senior and - 150",
+        "- and - and - and chicken 100",
+        "- and - and - and - 150"
+    )
+    Solution().solution(info, query)
 }
