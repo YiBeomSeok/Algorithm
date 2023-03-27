1c3017404589a22739e6559a83eca8db7b57c13c
bmsk <vv99911@gmail.com>
2023-03-27 22:09:25 +0900

    [Add] Inflearn 10-1

---
+++ src/main/kotlin/inflearn/kotlin/ch10/1 계단오르기.kt
@@ -0,0 +1,20 @@
+package inflearn.kotlin.ch10
+
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val n = br.readLine().toInt()
+
+    val dp = IntArray(n + 1)
+    dp[1] = 1
+    dp[2] = 2
+
+
+    for(i in 3 .. n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    print(dp[n])
+}
