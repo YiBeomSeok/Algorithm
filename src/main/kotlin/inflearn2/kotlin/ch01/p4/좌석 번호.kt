1c79c3fd350882d553326ae450f816592fda2b7f
bmsk <vv99911@gmail.com>
2023-03-29 10:22:01 +0900

    [Refactor] 좌석 번호 - 스레기 코드 수정

--- src/main/kotlin/inflearn2/kotlin/ch01/p4/좌석 번호.kt
+++ src/main/kotlin/inflearn2/kotlin/ch01/p4/좌석 번호.kt
@@ -3,48 +3,34 @@ package inflearn2.kotlin.ch01.p4
 import java.util.*
 
 class Solution {
-    val dx = intArrayOf(0, 1, 0, -1)
-    val dy = intArrayOf(1, 0, -1, 0)
-
-
     fun solution(c: Int, r: Int, k: Int): IntArray {
-        var dir = 0
         val answer = IntArray(2)
-        val visited = Array(r + 2) {
-            IntArray(c + 2)
-        }
-
-        visited[r + 1][1] = 1
-        visited[r][c + 1] = 1
-        visited[0][c] = 1
-
-        visited[1][1] = 1
-        var x = 1
-        var y = 1
-        var number = 1
-
-        while(number < k) {
-            number++
-
-            var nextX = x + dx[dir]
-            var nextY = y + dy[dir]
-            if(visited[nextY][nextX] == 1) {
-                dir = (dir + 1) % 4
-                nextX = x + dx[dir]
-                nextY = y + dy[dir]
+        if (k > c * r) return intArrayOf(0, 0)
+        val seat = Array(c) { IntArray(r) }
+        val dx = intArrayOf(-1, 0, 1, 0)
+        val dy = intArrayOf(0, 1, 0, -1)
+        var x = 0
+        var y = 0
+        var count = 1
+        var d = 1
+        while (count < k) {
+            val nx = x + dx[d]
+            val ny = y + dy[d]
+            if (nx < 0 || nx >= c || ny < 0 || ny >= r || seat[nx][ny] > 0) {
+                d = (d + 1) % 4
+                continue
             }
-
-            if(visited[nextY][nextX] == 1) return intArrayOf(0, 0)
-            visited[nextY][nextX] = 1
-            x = nextX
-            y = nextY
+            seat[x][y] = count
+            count++
+            x = nx
+            y = ny
         }
-
-        answer[0] = x
-        answer[1] = y
+        answer[0] = x + 1
+        answer[1] = y + 1
         return answer
     }
 }
+
 
 fun main() {
     val T = Solution()
