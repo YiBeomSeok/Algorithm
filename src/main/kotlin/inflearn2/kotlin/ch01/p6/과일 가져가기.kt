accc2ccd6e5ae0ab869468bb6b910132ff0cf318
bmsk <vv99911@gmail.com>
2023-03-29 16:03:06 +0900

    Inflearn2 시뮬

--- src/main/kotlin/inflearn2/kotlin/ch01/p6/과일 가져가기.kt
+++ src/main/kotlin/inflearn2/kotlin/ch01/p6/과일 가져가기.kt
@@ -1,6 +1,92 @@
 class Solution {
-    fun solution(fruit: Array<IntArray>?): Int {
-        return 0
+    fun solution(fruit: Array<IntArray>): Int {
+        var answer = 0
+        val n = fruit.size
+        val check = IntArray(n)
+
+        for (i in 0 until n) {
+            if(check[i] == 1) continue
+
+            // i의 최소 과일이 무엇인지 확인
+            //
+            // i의 최소값이 1개인지 확인
+            var iMinIndex = 0
+            for(iSub in 1 until 3) {
+                if(fruit[i][iSub] < fruit[i][iMinIndex]) {
+                    iMinIndex = iSub
+                }
+            }
+            var isUnique = 0
+            for(iSub in 0 until 3) {
+                if(fruit[i][iSub] == fruit[i][iMinIndex]) {
+                    isUnique++
+                }
+            }
+            if(isUnique > 1) {
+                answer += fruit[i][iMinIndex]
+                check[i]++
+                continue
+            }
+
+            for (j in i + 1 until n) {
+                if(check[j] == 1) continue
+                if(check[i] == 1) break
+
+                // j의 최소 과일이 무엇인지 확인
+                //
+                // j의 최소값이 1개인지 확인
+                var jMinIndex = 0
+                for(jSub in 1 until 3) {
+                    if(fruit[j][jSub] < fruit[j][jMinIndex]) {
+                        jMinIndex = jSub
+                    }
+                }
+                isUnique = 0
+                for(jSub in 0 until 3) {
+                    if(fruit[j][jSub] == fruit[j][jMinIndex]) {
+                        isUnique++
+                    }
+                }
+                if(isUnique > 1) {
+                    answer += fruit[j][jMinIndex]
+                    check[j]++
+                    continue
+                }
+
+                // i의 최소 과일과 j의 최소 과일이 다른지 확인
+                if(iMinIndex == jMinIndex) continue
+
+                // 교환 해보기
+                fruit[i][jMinIndex]--
+                fruit[i][iMinIndex]++
+                fruit[j][iMinIndex]--
+                fruit[j][jMinIndex]++
+
+                // 교환 후
+                // i의 현재 최소값을 확인해서 원래 최소값보다 증가했는지 확인
+                // j의 현재 최소값을 확인해서 원래 최소값보다 증가했는지 확인
+                if(fruit[i][jMinIndex] < fruit[i][iMinIndex] ||
+                    fruit[j][iMinIndex] < fruit[j][jMinIndex]) {
+                    fruit[i][jMinIndex]++
+                    fruit[i][iMinIndex]--
+                    fruit[j][iMinIndex]++
+                    fruit[j][jMinIndex]--
+
+                    continue
+                }
+
+                answer += fruit[i][iMinIndex] + fruit[j][jMinIndex]
+                check[i] = 1
+                check[j] = 1
+            }
+
+            if(check[i] == 0) {
+                answer += fruit[i][iMinIndex]
+            }
+            check[i] = 1
+        }
+
+        return answer
     }
 }
 
--- src/main/kotlin/inflearn2/kotlin/ch01/p7/비밀번호.kt
+++ src/main/kotlin/inflearn2/kotlin/ch01/p7/비밀번호.kt
@@ -1,12 +1,69 @@
 package inflearn2.kotlin.ch01.p7
 
+import java.util.*
 
-internal class Solution {
-    fun solution(keypad: IntArray?, password: String?): Int {
-        return 0
+class Point(val y: Int, val x: Int, var weight: Int): Comparable<Point> {
+    override fun compareTo(other: Point) = this.weight - other.weight
+}
+class Solution {
+    val dx = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
+    val dy = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
+
+    fun solution(keypad: IntArray, password: String): Int {
+        val check = Array(10) { IntArray(10) { Int.MAX_VALUE } }
+
+        val board = Array(4) { IntArray(4) }
+        var offset = 0
+        for(i in 1 .. 3) {
+            for(j in 1 .. 3) {
+                board[i][j] = keypad[offset++]
+            }
+        }
+
+        for(i in 1 .. 3) {
+            for(j in 1 ..3) {
+                val curNumber = board[i][j]
+
+                bfs(curNumber, i, j, board, check)
+            }
+        }
+
+        var answer = 0
+        var prev = password[0] - '0'
+        for(i in 1 until password.length) {
+            val cur = password[i] - '0'
+            answer += check[prev][cur]
+            prev = cur
+        }
+
+        return answer
     }
 
+    fun bfs(curNumber: Int, curY: Int, curX: Int, board: Array<IntArray>, check: Array<IntArray>) {
+        check[curNumber][curNumber] = 0
 
+        val pq = PriorityQueue<Point>()
+        pq.add(Point(curY, curX, 0))
+
+        while(pq.isNotEmpty()) {
+            val curPoint = pq.poll()
+
+            if(curPoint.weight > check[curNumber][board[curPoint.y][curPoint.x]]) continue
+
+            for(dir in 0 until 8) {
+                val nextY = curPoint.y + dy[dir]
+                val nextX = curPoint.x + dx[dir]
+
+                if(nextY == 0 || nextY == 4 ||
+                    nextX == 0 || nextX == 4) continue
+
+                if(curPoint.weight + 1 < check[curNumber][board[nextY][nextX]]) {
+                    check[curNumber][board[nextY][nextX]] = curPoint.weight + 1
+                    pq.add(Point(nextY, nextX, curPoint.weight + 1))
+                }
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
