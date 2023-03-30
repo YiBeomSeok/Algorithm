5dc74dc871d97bb2b5759edf4d1741bff53084cd
bmsk <vv99911@gmail.com>
2023-03-30 15:58:26 +0900

    Update 피부과.kt

--- src/main/kotlin/inflearn2/kotlin/ch03/p4/피부과.kt
+++ src/main/kotlin/inflearn2/kotlin/ch03/p4/피부과.kt
@@ -1,8 +1,82 @@
 package inflearn2.kotlin.ch03.p4
 
+import java.util.*
+
 class Solution {
     fun solution(laser: IntArray, enter: Array<String>): Int {
-        return 0
+        var answer = 0
+        val n: Int = enter.size
+        val inList = Array(n) { IntArray(2) }
+        for (i in 0 until n) {
+            val a = getTime(enter[i].split(" ")[0])
+            val b = enter[i].split(" ")[1].toInt()
+            inList[i][0] = a
+            inList[i][1] = b
+        }
+        val Q: Queue<Int> = LinkedList()
+        Q.offer(inList[0][1])
+        var fT = inList[0][0]
+        var pos = 1
+        for (t in fT..1200) {
+            if (pos < n && t == inList[pos][0]) {
+                if (Q.isEmpty() && inList[pos][0] > fT) fT = inList[pos][0]
+                Q.offer(inList[pos][1])
+                pos++
+            }
+            if (t == fT && !Q.isEmpty()) {
+                val idx = Q.poll()
+                fT += laser[idx]
+            }
+            answer = Math.max(answer, Q.size)
+        }
+        return answer
+    }
+
+    private fun getTime(timeString: String): Int {
+        val split = timeString.split(":")
+        return split[0].toInt() * 60 + split[1].toInt()
+    }
+}
+
+class Solution2 {
+    class Schedule(val time: Int, val state: String) : Comparable<Schedule> {
+        override fun compareTo(other: Schedule): Int {
+            return if (this.time == other.time) this.state.compareTo(other.state) else this.time - other.time
+        }
+    }
+
+    fun solution(laser: IntArray, enter: Array<String>): Int {
+        val schedules = mutableListOf<Schedule>()
+
+        for (info in enter) {
+            val split = info.split(" ")
+            val timeString = split[0]
+            val durationTime = laser[split[1].toInt()]
+            val start = getTime(timeString)
+            val end = start + durationTime
+
+            schedules.add(Schedule(start, "s"))
+            schedules.add(Schedule(end, "e"))
+        }
+
+        schedules.sort()
+
+        var answer = 0
+        var waitingCount = 0
+        for (schedule in schedules) {
+            if (schedule.state == "s") {
+                waitingCount++
+                answer = maxOf(waitingCount - 1, answer)
+            } else {
+                waitingCount--
+            }
+        }
+        return answer
+    }
+
+    private fun getTime(timeString: String): Int {
+        val split = timeString.split(":")
+        return split[0].toInt() * 60 + split[1].toInt()
     }
 }
 
