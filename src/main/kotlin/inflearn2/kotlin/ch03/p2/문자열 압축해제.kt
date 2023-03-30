e22f27ed30cad8ba38b423bb4c164ab359aaf248
bmsk <vv99911@gmail.com>
2023-03-30 15:58:22 +0900

    Update 문자열 압축해제.kt

--- src/main/kotlin/inflearn2/kotlin/ch03/p2/문자열 압축해제.kt
+++ src/main/kotlin/inflearn2/kotlin/ch03/p2/문자열 압축해제.kt
@@ -1,8 +1,44 @@
 package inflearn2.kotlin.ch03.p2
 
+import java.util.*
+
 class Solution {
     fun solution(s: String): String {
-        return ""
+        val stack = Stack<String>()
+        var answer = ""
+
+        s.forEach {
+            if(it == ')') {
+                var tmp = ""
+                while(stack.isNotEmpty()) {
+                    val c = stack.pop()
+                    if(c == "(") {
+                        var num = ""
+                        while(stack.isNotEmpty() && stack.peek()[0].isDigit()) {
+                            num = stack.pop() + num
+                        }
+                        var res = ""
+                        var cnt = 0
+                        if(num == "") cnt = 1
+                        else cnt = num.toInt()
+
+                        for(i in 0 until cnt) {
+                            res += tmp
+                        }
+
+                        stack.push(res)
+                        break
+                    }
+                    tmp = c + tmp
+                }
+            } else {
+                stack.push("$it")
+            }
+        }
+        stack.forEach {
+            answer += it
+        }
+        return answer
     }
 }
 
