572c5d8b4d7d246163b92fd9b12f3b3975cf4b64
bmsk <vv99911@gmail.com>
2023-03-31 19:10:43 +0900

    Update 가장 많이 사용된 회의실.kt

--- src/main/kotlin/inflearn2/kotlin/ch03/p6/가장 많이 사용된 회의실.kt
+++ src/main/kotlin/inflearn2/kotlin/ch03/p6/가장 많이 사용된 회의실.kt
@@ -1,8 +1,57 @@
 package inflearn2.kotlin.ch03.p6
 
-internal class Solution {
+import java.util.*
+
+class Solution {
     fun solution(n: Int, meetings: Array<IntArray>): Int {
-        return 0
+        var answer = 0
+        val res = IntArray(n)
+        val ends = PriorityQueue(compareBy<IntArray> { it[0] }.thenBy { it[1] })
+        val rooms = TreeSet<Int>()
+
+        meetings.sortBy { it[0] }
+
+        for((i, value) in meetings.withIndex()) {
+            while(ends.isNotEmpty()) {
+                if(value[0] >= ends.peek()[0]) {
+                    rooms.remove(ends.poll()[1])
+                }
+                else break
+            }
+
+            var endTime = value[1]
+            val duration = value[1] - value[0]
+            if(ends.size == n) {
+                val first = ends.first()
+                endTime = first[0] + duration
+                while(ends.isNotEmpty()) {
+                    if(ends.peek()[0] == first[0]) {
+                        rooms.remove(ends.poll()[1])
+                    } else {
+                        break
+                    }
+                }
+            }
+
+            for(i in 0 until n) {
+                if(!rooms.contains(i)) {
+                    rooms.add(i)
+                    ends.offer(intArrayOf(endTime, i))
+                    res[i]++
+                    break
+                }
+            }
+        }
+
+        var max = 0
+        for((i, value) in res.withIndex()) {
+            if(max < value) {
+                max = value
+                answer = i
+            }
+        }
+
+        return answer
     }
 }
 
