268d4282e3af4ab6738e8407835dce6939e8e2e2
bmsk <vv99911@gmail.com>
2023-03-31 19:10:39 +0900

    Update cpu 스케쥴링.kt

--- src/main/kotlin/inflearn2/kotlin/ch03/p5/cpu 스케쥴링.kt
+++ src/main/kotlin/inflearn2/kotlin/ch03/p5/cpu 스케쥴링.kt
@@ -2,9 +2,47 @@ package inflearn2.kotlin.ch03.p5
 
 import java.util.*
 
-internal class Solution {
+class Task(val number: Int, val duration: Int) : Comparable<Task> {
+    override fun compareTo(other: Task): Int {
+        return if (duration == other.duration) number - other.number else duration - other.duration
+    }
+}
+
+class Solution {
+    // 틀린 풀이라고 할 수 있는 비효율(까진 아니지만 더 좋은 코드 존재)
     fun solution(tasks: Array<IntArray>): IntArray {
-        return intArrayOf()
+        val answer = IntArray(tasks.size)
+        val taskHashMap = HashMap<Int, MutableList<Task>>()
+        val waitingPQ = PriorityQueue<Task>()
+
+        for ((i, value) in tasks.withIndex()) {
+            taskHashMap[value[0]] = taskHashMap.getOrDefault(value[0], mutableListOf()).apply { add(Task(i, value[1])) }
+        }
+
+        var finishTime = 0
+        var time = 0
+        var processed = 0
+        while(processed < tasks.size) {
+            if(taskHashMap.contains(time)) {
+                taskHashMap[time]?.forEach {
+                    waitingPQ.add(it)
+                }
+            }
+            if(time == finishTime || finishTime == 0) {
+                if(waitingPQ.isNotEmpty()) {
+                    val cpuTask = waitingPQ.poll()
+                    answer[processed] = cpuTask.number
+                    processed++
+                    finishTime = time + cpuTask.duration
+                } else {
+                    finishTime = 0
+                }
+            }
+
+            //
+            time++
+        }
+        return answer
     }
 }
 
