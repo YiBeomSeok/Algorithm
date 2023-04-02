84b05c598e3fd29388769819f85b06664d9634c0
bmsk <vv99911@gmail.com>
2023-04-02 14:14:17 +0900

    Update 가장 가까운 큰 수.kt

--- src/main/kotlin/inflearn2/kotlin/ch06/p1/가장 가까운 큰 수.kt
+++ src/main/kotlin/inflearn2/kotlin/ch06/p1/가장 가까운 큰 수.kt
@@ -1,8 +1,38 @@
 package inflearn2.kotlin.ch06.p1
 
 class Solution {
+
+    private lateinit var candidateNumbers: IntArray
+    private lateinit var visited: BooleanArray
+    private var answer = -1
+
     fun solution(n: Int): Int {
-        return 0
+        init(n)
+        dfs(0, n, 0)
+        return answer
+    }
+
+    private fun init(n: Int) {
+        candidateNumbers = n.toString().toCharArray().map { it - '0' }.sorted().toIntArray()
+        visited = BooleanArray(candidateNumbers.size) { false }
+        answer = -1
+    }
+
+    private fun dfs(currentNumber: Int, targetNumber: Int, depth: Int) {
+        if(answer != -1) return
+
+        if (currentNumber > targetNumber) {
+            answer = currentNumber
+            return
+        }
+
+        candidateNumbers.forEachIndexed { index, num ->
+            if (!visited[index]) {
+                visited[index] = true
+                dfs(currentNumber * 10 + num, targetNumber, depth + 1)
+                visited[index] = false
+            }
+        }
     }
 }
 
