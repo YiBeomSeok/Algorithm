aac025037b3447491856e7e936cb7bbf22fc9053
bmsk <vv99911@gmail.com>
2023-04-02 21:44:09 +0900

    Update IP 주소.kt

--- src/main/kotlin/inflearn2/kotlin/ch06/p5/IP 주소.kt
+++ src/main/kotlin/inflearn2/kotlin/ch06/p5/IP 주소.kt
@@ -3,8 +3,39 @@ package inflearn2.kotlin.ch06.p5
 import java.util.*
 
 class Solution {
+
+    lateinit var answer: MutableList<String>
+    lateinit var tmp: LinkedList<String>
+
     fun solution(s: String): Array<String> {
-        return arrayOf()
+
+        init(s)
+
+        dfs(0, s)
+
+        return answer.toTypedArray()
+    }
+
+    private fun init(s: String) {
+        answer = mutableListOf()
+        tmp = LinkedList()
+    }
+
+    private fun dfs(start: Int, s: String) {
+        if(tmp.size == 4 && start == s.length) {
+            var tmpStr = ""
+            for(x in tmp) tmpStr += "$x."
+            answer.add(tmpStr.dropLast(1))
+        } else {
+            for(i in start until s.length) {
+                if(s[start] == '0' && i > start) return
+                val num = s.substring(start, i + 1)
+                if(num.toInt() > 255) return
+                tmp.add(num)
+                dfs(i + 1, s)
+                tmp.pollLast()
+            }
+        }
     }
 }
 
