6f0298a40911a5b2da64d5881440e74c12fe8136
bmsk <vv99911@gmail.com>
2023-04-03 14:43:51 +0900

    Update 타일점프.kt

--- src/main/kotlin/inflearn2/kotlin/ch07/p1/타일점프.kt
+++ src/main/kotlin/inflearn2/kotlin/ch07/p1/타일점프.kt
@@ -2,7 +2,34 @@ package inflearn2.kotlin.ch07.p1
 
 class Solution {
     fun solution(nums: IntArray): Int {
-        return 0
+        return bfs(nums)
+    }
+
+    private fun bfs(nums: IntArray): Int {
+        val visited = IntArray(nums.size) { 0 }
+        val queue = ArrayDeque<Int>()
+        val result = -1
+
+        queue.addLast(0)
+
+        while (queue.isNotEmpty()) {
+            val curTile = queue.removeFirst()
+
+            if (curTile == nums.size - 1) return visited[curTile]
+            if (nums[curTile] == 0) continue
+
+            for (i in 1 .. nums[curTile]) {
+                val nextTile = curTile + i
+
+                if (nextTile >= nums.size) break
+                else if (visited[nextTile] != 0) continue
+
+                visited[nextTile] += visited[curTile] + 1
+                queue.add(nextTile)
+            }
+        }
+
+        return result
     }
 }
 
