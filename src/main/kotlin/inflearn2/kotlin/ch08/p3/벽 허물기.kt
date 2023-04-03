6d0b43f1119ef35c232a674a58d7aab5e34b7dcc
bmsk <vv99911@gmail.com>
2023-04-03 21:52:26 +0900

    Update 벽 허물기.kt

--- src/main/kotlin/inflearn2/kotlin/ch08/p3/벽 허물기.kt
+++ src/main/kotlin/inflearn2/kotlin/ch08/p3/벽 허물기.kt
@@ -1,7 +1,34 @@
 package inflearn2.kotlin.ch08.p3
 
+import java.util.*
+
 class Solution {
+    val dx = intArrayOf(0, 1, 0, -1)
+    val dy = intArrayOf(-1, 0, 1, 0)
+
     fun solution(board: Array<IntArray>): Int {
+        val pq = PriorityQueue<IntArray>( compareBy { it[2] })
+        pq.offer(intArrayOf(0, 0, 0))
+
+        while(pq.isNotEmpty()) {
+            val curPos = pq.poll()
+            if(curPos[0] == board.size - 1 && curPos[1] == board[0].size - 1) {
+                return curPos[2]
+            }
+
+            for(dir in 0 until 4) {
+                val nY = curPos[0] + dy[dir]
+                val nX = curPos[1] + dx[dir]
+
+                if(nY in board.indices && nX in board[0].indices && board[nY][nX] != -1) {
+                    if(board[nY][nX] == 1)
+                        pq.offer(intArrayOf(nY, nX, curPos[2] + 1))
+                    else
+                        pq.offer(intArrayOf(nY, nX, curPos[2]))
+                    board[nY][nX] = -1
+                }
+            }
+        }
         return 0
     }
 }
