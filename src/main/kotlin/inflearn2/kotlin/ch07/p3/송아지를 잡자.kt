10dedda67dd0509492abbb4e07026ace97a2d424
bmsk <vv99911@gmail.com>
2023-04-03 14:43:55 +0900

    Update 송아지를 잡자.kt

--- src/main/kotlin/inflearn2/kotlin/ch07/p3/송아지를 잡자.kt
+++ src/main/kotlin/inflearn2/kotlin/ch07/p3/송아지를 잡자.kt
@@ -1,8 +1,40 @@
 package inflearn2.kotlin.ch07.p3
 
 class Solution {
+    lateinit var visited: Array<IntArray>
+
     fun solution(s: Int, e: Int): Int {
-        return 0
+        visited = Array(2) { IntArray(200001) }
+
+        return bfs(s, e)
+    }
+
+    private fun bfs(s: Int, e: Int): Int {
+        val deque = ArrayDeque<Int>()
+        var cow = e
+        deque.addLast(s)
+        visited[0][s] = 1
+
+        var depth = 0
+        while (deque.isNotEmpty()) {
+            val len = deque.size
+            depth++
+
+            for (i in 0 until len) {
+                val curX = deque.removeFirst()
+                for (nextX in intArrayOf(curX - 1, curX + 1, curX * 2)) {
+                    if (nextX in 0..200000 && visited[depth % 2][nextX] == 0) {
+                        visited[depth % 2][nextX] = 1
+                        deque.addLast(nextX)
+                    }
+                }
+            }
+            cow += depth
+            if (cow > 200000) return -1
+            if (visited[depth % 2][cow] == 1) return depth
+        }
+
+        return -1
     }
 }
 
