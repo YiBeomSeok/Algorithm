59732fb4678f38eb3f0613caa89f3819b7c90211
bmsk <vv99911@gmail.com>
2023-04-03 22:59:47 +0900

    Update 방향 바꾸기.kt

--- src/main/kotlin/inflearn2/kotlin/ch08/p4/방향 바꾸기.kt
+++ src/main/kotlin/inflearn2/kotlin/ch08/p4/방향 바꾸기.kt
@@ -1,8 +1,44 @@
 package inflearn2.kotlin.ch08.p4
 
+import java.util.*
+
 class Solution {
+    val dx = intArrayOf(0, 1, -1, 0, 0)
+    val dy = intArrayOf(0, 0, 0, 1, -1)
+
     fun solution(board: Array<IntArray>): Int {
-        return 0
+
+        val visited = Array(board.size) { IntArray(board[0].size) }
+
+        val pq = PriorityQueue<IntArray>(compareBy { it[2] })
+        pq.offer(intArrayOf(0, 0, 0))
+
+        while (pq.isNotEmpty()) {
+            val curPos = pq.poll()
+
+            if (curPos[0] == board.size - 1 && curPos[1] == board[0].size - 1) {
+                return curPos[2]
+            }
+
+            if(visited[curPos[0]][curPos[1]] == 1) continue
+            visited[curPos[0]][curPos[1]] = 1
+
+            for (dir in 1 until 5) {
+                val nY = curPos[0] + dy[dir]
+                val nX = curPos[1] + dx[dir]
+
+                if (nY in board.indices && nX in board[0].indices && visited[nY][nX] == 0) {
+                    if (dir != board[curPos[0]][curPos[1]]) {
+                        pq.add(intArrayOf(nY, nX, curPos[2] + 1))
+                    } else {
+                        pq.add(intArrayOf(nY, nX, curPos[2]))
+                    }
+                }
+            }
+
+        }
+
+        return -1
     }
 }
 
