ffb0018f2191748a09e266083f4b0d1c4e1719d9
bmsk <vv99911@gmail.com>
2023-04-04 19:58:51 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 32.46 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.kt
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.kt
@@ -1,4 +1,3 @@
-import java.util.*
 import kotlin.math.*
 
 class Solution {
@@ -6,44 +5,53 @@ class Solution {
         val answer = IntArray(numbers.size)
 
         numbers.forEachIndexed { index, value ->
-            var bin = value.toString(2)
-            // 포화 이진트리의 노드 개수
-            val allNodeCount = findAllNodeCount(bin)
-            while(bin.length < allNodeCount) {
-                bin = "0$bin"
+            var binaryString = value.toString(2)
+            // Calculate the number of nodes in a complete binary tree
+            val totalNodes = findTotalNodeCount(binaryString)
+
+            // Pad the binary string with zeros until it reaches the total node count
+            while (binaryString.length < totalNodes) {
+                binaryString = "0$binaryString"
             }
-            answer[index] = canMakeBinaryTree('1', bin)
+
+            answer[index] = canFormBinaryTree('1', binaryString)
         }
 
         return answer
     }
 
-    private fun findAllNodeCount(bin: String): Int {
+    private fun findTotalNodeCount(binaryString: String): Int {
         val base = 2.0
-        var x = 1
-        var count = (base.pow(x) - 1)
-        while(bin.length > count) {
-            count = (base.pow(++x) - 1)
+        var exponent = 0
+
+        // Find the smallest exponent for which 2^exponent - 1 is greater than the binary string length
+        while ((base.pow(++exponent) - 1) < binaryString.length) {
         }
-        return count.toInt()
+
+        return (base.pow(exponent) - 1).toInt()
     }
 
-    private fun canMakeBinaryTree(parent: Char, bin: String): Int {
-        if(bin.length == 1) {
-            if(bin[0] == '1') {
-                if(parent == '0') return 0
-                else return 1
-            } else return 1
+
+    private fun canFormBinaryTree(parent: Char, binaryString: String): Int {
+        if (binaryString.length == 1) {
+            return if (binaryString[0] == '1') {
+                if (parent == '0') 0
+                else 1
+            } else 1
         }
-        val midIndex = bin.length / 2
-        val mid = bin[midIndex]
-        
-        if(mid == '1' && parent == '0') return 0
-        
-        if(canMakeBinaryTree(mid, bin.substring(0, midIndex)) == 0 || canMakeBinaryTree(mid, bin.substring(midIndex + 1, bin.length)) == 0) {
+
+        val midIndex = binaryString.length / 2
+        val mid = binaryString[midIndex]
+
+        if (mid == '1' && parent == '0') return 0
+
+        val leftSubtree = binaryString.substring(0, midIndex)
+        val rightSubtree = binaryString.substring(midIndex + 1, binaryString.length)
+
+        if (canFormBinaryTree(mid, leftSubtree) == 0 || canFormBinaryTree(mid, rightSubtree) == 0) {
             return 0
         }
-        
+
         return 1
     }
 }
