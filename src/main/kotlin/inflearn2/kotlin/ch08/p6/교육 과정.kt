04e806b5133893996532468befb5d26d273d9c01
bmsk <vv99911@gmail.com>
2023-04-04 23:04:57 +0900

    Update 교육 과정.kt

--- src/main/kotlin/inflearn2/kotlin/ch08/p6/교육 과정.kt
+++ src/main/kotlin/inflearn2/kotlin/ch08/p6/교육 과정.kt
@@ -1,11 +1,50 @@
 package inflearn2.kotlin.ch08.p6
 
+import java.util.PriorityQueue
+
 class Solution {
     fun solution(
         subjects: Array<String>,
         course: Array<String>
     ): Array<String> {
-        return arrayOf()
+        val n = subjects.size
+        val node = HashMap<String, Int>()
+        subjects.forEachIndexed { i, s ->
+            node[s] = i
+        }
+        val graph = Array(n) { mutableListOf<Int>() }
+        val indegree = IntArray(n)
+
+        course.forEach {
+            with(it.split(" ")) {
+                val from = node[this[1]]!!
+                val to = node[this[0]]!!
+
+                graph[from].add(to)
+                indegree[to]++
+            }
+        }
+
+        val order = mutableListOf<Int>()
+        val queue = ArrayDeque<Int>()
+        indegree.forEachIndexed { i, v -> if (v == 0) queue.addLast(i) }
+
+        while (queue.isNotEmpty()) {
+            val curFrom = queue.removeFirst()
+            order.add(curFrom)
+            graph[curFrom].forEach { to ->
+                indegree[to]--
+                if (indegree[to] == 0) {
+                    queue.addLast(to)
+                }
+            }
+        }
+
+        val answer = Array(n) {
+            subjects[order[it]]
+        }
+
+        return answer
     }
 }
 
@@ -15,18 +54,18 @@ fun main(args: Array<String>) {
         T.solution(
             arrayOf("english", "math", "physics", "art", "music"),
             arrayOf("art math", "physics art", "art music", "physics math", "english physics")
-        )
+        ).joinToString()
     )
-//    println(
-//        T.solution(
-//            arrayOf("art", "economics", "history", "chemistry"),
-//            arrayOf("chemistry history", "economics history", "art economics")
-//        )[0]
-//    )
-//    println(
-//        T.solution(
-//            arrayOf("math", "science", "music", "biology"),
-//            arrayOf("science music", "math music", "math science", "biology math")
-//        )
-//    )
+    println(
+        T.solution(
+            arrayOf("art", "economics", "history", "chemistry"),
+            arrayOf("chemistry history", "economics history", "art economics")
+        ).joinToString()
+    )
+    println(
+        T.solution(
+            arrayOf("math", "science", "music", "biology"),
+            arrayOf("science music", "math music", "math science", "biology math")
+        ).joinToString()
+    )
 }
