2b1d6a90885b79e5ac3218d3555701b4a559bd73
bmsk <vv99911@gmail.com>
2023-04-05 13:05:00 +0900

    Update 비밀번호.kt

--- src/main/kotlin/inflearn2/kotlin/ch01/p7/비밀번호.kt
+++ src/main/kotlin/inflearn2/kotlin/ch01/p7/비밀번호.kt
@@ -1,110 +1,39 @@
 package inflearn2.kotlin.ch01.p7
 
-import java.util.*
-
-class Point(val y: Int, val x: Int, var weight: Int): Comparable<Point> {
-    override fun compareTo(other: Point) = this.weight - other.weight
-}
 class Solution {
     val dx = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
     val dy = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
 
-    fun solution2(keypad: IntArray, password: String): Int {
-        var answer = 0
-        val dx = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
-        val dy = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
-        val pad = Array(3) { IntArray(3) }
-        val dist = Array(10) { IntArray(10) }
+    fun solution(keypad: IntArray, password: String): Int {
+        val dist = Array(10) { IntArray(10) { 2 } }
+        val board = Array(10) { IntArray(10) }
 
-        for (i in 0..2) {
-            for (j in 0..2) {
-                pad[i][j] = keypad[i * 3 + j]
-            }
+        keypad.forEachIndexed { i, num ->
+            board[i / 3][i % 3] = num
         }
 
-        for (i in 0..9) {
-            dist[i].fill(2)
-        }
+        for(i in board.indices) {
+            for(j in board[i].indices) {
+                for(dir in 0 until 8) {
+                    val nY = i + dy[dir]
+                    val nX = j + dx[dir]
 
-        for (i in 0..9) {
-            dist[i][i] = 0
-        }
-
-        for (i in 0..2) {
-            for (j in 0..2) {
-                val from = pad[i][j]
-                for (k in 0..7) {
-                    if (i + dx[k] >= 0 && i + dx[k] < 3 && j + dy[k] >= 0 && j + dy[k] < 3) {
-                        val to = pad[i + dx[k]][j + dy[k]]
-                        dist[from][to] = 1
+                    if (nY in board.indices && nX in board.indices) {
+                        dist[board[i][j]][board[nY][nX]] = 1
                     }
                 }
             }
         }
 
-        for (i in 1 until password.length) {
-            val from = password[i - 1] - '0'
-            val to = password[i] - '0'
-            answer += dist[from][to]
-        }
-
-        return answer
-    }
-
-    fun solution(keypad: IntArray, password: String): Int {
-        val check = Array(10) { IntArray(10) { Int.MAX_VALUE } }
-
-        val board = Array(4) { IntArray(4) }
-        var offset = 0
-        for(i in 1 .. 3) {
-            for(j in 1 .. 3) {
-                board[i][j] = keypad[offset++]
-            }
-        }
-
-        for(i in 1 .. 3) {
-            for(j in 1 ..3) {
-                val curNumber = board[i][j]
-
-                bfs(curNumber, i, j, board, check)
-            }
-        }
-
         var answer = 0
-        var prev = password[0] - '0'
+
+        var prevNum = password[0] - '0'
         for(i in 1 until password.length) {
-            val cur = password[i] - '0'
-            answer += check[prev][cur]
-            prev = cur
+            answer += dist[prevNum][password[i] - '0']
+            prevNum = password[i] - '0'
         }
 
         return answer
-    }
-
-    fun bfs(curNumber: Int, curY: Int, curX: Int, board: Array<IntArray>, check: Array<IntArray>) {
-        check[curNumber][curNumber] = 0
-
-        val pq = PriorityQueue<Point>()
-        pq.add(Point(curY, curX, 0))
-
-        while(pq.isNotEmpty()) {
-            val curPoint = pq.poll()
-
-            if(curPoint.weight > check[curNumber][board[curPoint.y][curPoint.x]]) continue
-
-            for(dir in 0 until 8) {
-                val nextY = curPoint.y + dy[dir]
-                val nextX = curPoint.x + dx[dir]
-
-                if(nextY == 0 || nextY == 4 ||
-                    nextX == 0 || nextX == 4) continue
-
-                if(curPoint.weight + 1 < check[curNumber][board[nextY][nextX]]) {
-                    check[curNumber][board[nextY][nextX]] = curPoint.weight + 1
-                    pq.add(Point(nextY, nextX, curPoint.weight + 1))
-                }
-            }
-        }
     }
 }
 
