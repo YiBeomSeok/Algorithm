45db5dce57ef2f5c0bdf6bc5aca659abaa6cdad4
bmsk <vv99911@gmail.com>
2023-04-12 18:44:05 +0900

    [level 3] Title: 외벽 점검, Time: 5.86 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60062. 외벽 점검/외벽 점검.kt
@@ -0,0 +1,44 @@
+class Solution {
+    private var minFriends: Int = 0
+
+    fun solution(n: Int, weak: IntArray, dist: IntArray): Int {
+        minFriends = n + 1
+        val extendedWeak = IntArray(weak.size * 2)
+        var idx = 0
+
+        for (i in weak.indices) {
+            extendedWeak[i] = weak[i]
+            extendedWeak[i + weak.size] = weak[i] + n
+        }
+
+        for (i in weak.indices) {
+            checkWalls(extendedWeak, dist, BooleanArray(dist.size), n, i, weak.size + i, 0)
+        }
+
+        return if (minFriends > n) -1 else minFriends
+    }
+
+    private fun checkWalls(
+        extendedWeak: IntArray, dist: IntArray, visited: BooleanArray,
+        n: Int, start: Int, end: Int, friendsUsed: Int
+    ) {
+        if (start >= end) {
+            minFriends = minFriends.coerceAtMost(friendsUsed)
+            return
+        }
+
+        for (i in dist.indices) {
+            if (visited[i]) continue
+
+            visited[i] = true
+            var idx = start + 1
+
+            while (idx < end && extendedWeak[idx] - extendedWeak[start] <= dist[i]) {
+                idx++
+            }
+
+            checkWalls(extendedWeak, dist, visited, n, idx, end, friendsUsed + 1)
+            visited[i] = false
+        }
+    }
+}
